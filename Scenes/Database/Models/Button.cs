8ff0dfe2d558dc28a5faa0cf4879da32753c4400
Nathan Etave <nathan.etave@gmail.com>
2024-10-15 15:13:32 +0200

    feat: add database models for Scenario, Page, Button, Image, Text, Video, Audio, and Model

---
+++ Scenes/Database/Models/Button.cs
@@ -0,0 +1,12 @@
+﻿using SQLite;
+using Tabloulet.Helpers;
+
+namespace Tabloulet.Scenes.Database.Models
+{
+    [Table(Constants.ButtonTable)]
+    public class Button : Base
+    {
+        public string Content { get; set; }
+        public string Color { get; set; }
+    }
+}
---
+++ Scenes/Database/Models/Image.cs
@@ -0,0 +1,11 @@
+﻿using SQLite;
+using Tabloulet.Helpers;
+
+namespace Tabloulet.Scenes.Database.Models
+{
+    [Table(Constants.ImageTable)]
+    public class Image : Base
+    {
+        public string Path { get; set; }
+    }
+}
