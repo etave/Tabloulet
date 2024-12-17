c3b47647337a3dbac4a30ee6cc878f60913e2b8e
RomainMechain <romain.mechain@etu.univ-orleans.fr>
2024-12-17 10:59:44 +0100

    feat: Added scenario reset button

--- Scenes/Viewer/Viewer.cs
+++ Scenes/Viewer/Viewer.cs
@@ -12,6 +12,8 @@ namespace Tabloulet.Scenes.ViewerNS
 
         private Guid _currentPage;
 
+        private Guid _idScenario;
+
         private ScenarioLoader _scenarioLoader;
 
         public override void _Ready()
@@ -26,6 +28,53 @@ namespace Tabloulet.Scenes.ViewerNS
         public void Init(Guid idScenario)
         {
             _currentPage = _scenarioLoader.LoadScenario(idScenario);
+            _idScenario = idScenario;
+            InitButtons();
+        }
+
+        public void InitButtons()
+        {
+            HBoxContainer hBoxContainer = new();
+            hBoxContainer.SetAnchorsPreset(LayoutPreset.TopLeft);
+            hBoxContainer.AddThemeConstantOverride("separation", 20);
+
+            StyleBoxFlat normalStyleBox = new() { BgColor = new Color(1, 1, 1, 0) };
+
+            Godot.Button buttonExit = new();
+            Texture2D texture2D = GD.Load<Texture2D>("res://Assets/Viewer/exit.png");
+            buttonExit.Icon = texture2D;
+            buttonExit.ZIndex = 101;
+            buttonExit.AddThemeConstantOverride("icon_max_width", 75);
+            buttonExit.AddThemeStyleboxOverride("normal", normalStyleBox);
+            buttonExit.AddThemeStyleboxOverride("hover", normalStyleBox);
+            buttonExit.AddThemeStyleboxOverride("pressed", normalStyleBox);
+            buttonExit.AddThemeStyleboxOverride("focus", normalStyleBox);
+
+            Godot.Button buttonReset = new();
+            texture2D = GD.Load<Texture2D>("res://Assets/Viewer/reset.png");
+            buttonReset.Icon = texture2D;
+            buttonReset.ZIndex = 101;
+            buttonReset.AddThemeConstantOverride("icon_max_width", 75);
+            buttonReset.AddThemeStyleboxOverride("normal", normalStyleBox);
+            buttonReset.AddThemeStyleboxOverride("hover", normalStyleBox);
+            buttonReset.AddThemeStyleboxOverride("pressed", normalStyleBox);
+            buttonReset.AddThemeStyleboxOverride("focus", normalStyleBox);
+            buttonReset.Pressed += ResetScenario;
+
+            hBoxContainer.AddChild(buttonExit);
+            hBoxContainer.AddChild(buttonReset);
+            AddChild(hBoxContainer);
+        }
+
+        private void ResetScenario()
+        {
+            Scenario scenario = _database.GetById<Scenario>(_idScenario);
+            Guid firstPage = scenario.PageId;
+            if (firstPage == _currentPage)
+            {
+                return;
+            }
+            ChangePage(scenario.PageId);
         }
 
         public Control GetDisplayRoot()
@@ -55,6 +104,7 @@ namespace Tabloulet.Scenes.ViewerNS
             FreePage();
             _currentPage = idPage;
             _scenarioLoader.LoadPage(_database.GetById<Page>(_currentPage));
+            InitButtons();
         }
     }
 }
