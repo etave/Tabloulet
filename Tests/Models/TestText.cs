1da812c402d702237a5602f504100c6e6e81b628
RomainMechain <romain.mechain@etu.univ-orleans.fr>
2025-01-13 14:44:58 +0100

    feat: Added unit tests for models

---
+++ Tests/Models/TestText.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using NUnit.Framework;
+using Tabloulet.DatabaseNS.Models;
+
+namespace Tabloulet.Tests.Models
+{
+    [TestFixture]
+    internal class TestText
+    {
+        private Text _text;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _text = new Text();
+        }
+
+        [Test]
+        public void Test_TextContentProperty()
+        {
+            // Arrange
+            var expectedContent = "Hello, World!";
+
+            // Act
+            _text.Content = expectedContent;
+
+            // Assert
+            Assert.AreEqual(
+                expectedContent,
+                _text.Content,
+                "The Content property should be set correctly."
+            );
+        }
+
+        [Test]
+        public void Test_TextFontProperty()
+        {
+            // Arrange
+            var expectedFont = "Arial";
+
+            // Act
+            _text.Font = expectedFont;
+
+            // Assert
+            Assert.AreEqual(expectedFont, _text.Font, "The Font property should be set correctly.");
+        }
+
+        [Test]
+        public void Test_TextFontSizeProperty()
+        {
+            // Arrange
+            var expectedFontSize = 12;
+
+            // Act
+            _text.FontSize = expectedFontSize;
+
+            // Assert
+            Assert.AreEqual(
+                expectedFontSize,
+                _text.FontSize,
+                "The FontSize property should be set correctly."
+            );
+        }
+    }
+}
